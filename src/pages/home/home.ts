0208d4b5f930003ad0b53037f6ed94eec3095703
AJ Beckner <ajbeckner@ajbeckner.com>
2017-04-23 13:29:17 -0400

    init commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  searchQuery: string = '';
+  items: string[];
+
+  constructor(public http: Http) {
+    this.initializeItems();
+  }
+
+  initializeItems() {
+    this.items = [
+	'Amsterdam',
+	'Bogota'
+    ];
+  }
+
+  getItems(ev: any) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+//    if (val && val.trim() != '') {
+//      this.items = this.items.filter((item) => {
+//        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+//      })
+//    }
+     if (val && val.trim() != '') {
+       this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
+        this.items = data.data.children;
+    });
+     }
+  }
+
+
+}
